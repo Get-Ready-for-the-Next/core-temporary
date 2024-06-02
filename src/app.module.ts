446d60156f1e7259949f9210f5ba5854ab0fb2c9
GwangCheonLee <raitmid56@gmail.com>
2024-06-02 14:32:20 +0900

    feat: App Controller and Service

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeormConfig } from './common/config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchemaConfig } from './common/config/validation.config';
+import { getEnvPath } from './common/config/environment-path.util';
+
+@Module({
+  imports: [
+    // AuthModule,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfig,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: getEnvPath(),
+      validationSchema: validationSchemaConfig(),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
